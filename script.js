71b9bc41827f7d9fd39363b99ae04bbe7b5ae59d
Linalleks <a_alleks@mail.ru>
2021-12-07 17:09:16 +0700

    change script file

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("Hello");
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+alert("Сообщение в модальном окне");
+
+console.log("Сообщение в консоли");
